955f91efff52a7b52759e257f987debe5c548525
david.hernando <david.hernandojurado@mytheresa.com>
2019-11-01 08:41:35 +0100

    introduced currentUserId context extension

---
+++ src/main/kotlin/io/realworld/conduit/user/infrstructure/api/ContextExtensions.kt
@@ -0,0 +1,10 @@
+package io.realworld.conduit.user.infrstructure.api
+
+import io.javalin.http.Context
+import io.realworld.conduit.shared.domain.ConduitException
+import io.realworld.conduit.user.domain.UserId
+
+
+fun Context.currentUserId(): UserId {
+    return attribute("userId") ?: throw ConduitException("User not logged in")
+}
--- src/main/kotlin/io/realworld/conduit/user/infrstructure/api/CurrentUserHandler.kt
+++ src/main/kotlin/io/realworld/conduit/user/infrstructure/api/CurrentUserHandler.kt
@@ -4,13 +4,11 @@ import io.javalin.http.Context
 import io.realworld.conduit.user.application.UserResponseBody
 import io.realworld.conduit.user.application.ViewCurrentUserRequest
 import io.realworld.conduit.user.application.ViewCurrentUserService
-import io.realworld.conduit.user.domain.UserId
 
 class CurrentUserHandler(private val viewCurrentUserService: ViewCurrentUserService) {
     fun handle(ctx: Context) {
-        val request = ViewCurrentUserRequest(userId = ctx.attribute<UserId>("userId")!!)
+        val request = ViewCurrentUserRequest(userId = ctx.currentUserId())
         val response = viewCurrentUserService.execute(request)
-
         ctx.json(UserResponseBody(user = response))
     }
 }
