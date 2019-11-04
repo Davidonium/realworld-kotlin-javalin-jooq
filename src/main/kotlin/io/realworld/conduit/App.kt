4439e00405287a9a767b441de773d6642b5157c6
david.hernando <david.hernandojurado@mytheresa.com>
2019-11-04 07:59:21 +0100

    App is now a class

--- src/main/kotlin/io/realworld/conduit/App.kt
+++ src/main/kotlin/io/realworld/conduit/App.kt
@@ -6,18 +6,54 @@ import io.realworld.conduit.profile.infrastructure.injection.profileModule
 import io.realworld.conduit.shared.infrastructure.injection.mainModule
 import io.realworld.conduit.user.infrastructure.injection.userModule
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import javax.sql.DataSource
 
 fun main() {
-    System.setProperty("user.timezone", "UTC")
+    App().start()
+}
 
-    val app = startKoin {
+class App(
+    private val properties: Map<String, Any> = mapOf(),
+    private val initializeDatabase: Boolean = false
+) {
+
+    private val container = startKoin {
         fileProperties()
+        environmentProperties()
+        properties(properties)
         modules(listOf(
             mainModule,
             articleModule,
             userModule,
             profileModule
         ))
+    }.koin
+
+    private val app = container.get<Javalin>()
+
+    init {
+        System.setProperty("user.timezone", "UTC")
     }
-    app.koin.get<Javalin>().start()
+
+    fun start() {
+        if (initializeDatabase) {
+            val initSql = Thread.currentThread()
+                .contextClassLoader
+                .getResourceAsStream("db/init.sql")
+                ?.reader().use { it?.readText() }
+            container.get<DataSource>().connection.use {
+                it.prepareStatement(initSql).execute()
+            }
+        }
+
+        app.start()
+    }
+
+    fun stop() {
+        app.stop()
+        stopKoin()
+    }
+
+    fun port() = app.port()
 }
