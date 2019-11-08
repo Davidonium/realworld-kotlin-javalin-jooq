145c9c3c7184c9d94ff35e7e553152dc63f6e3be
david.hernando <david.hernandojurado@mytheresa.com>
2019-11-08 20:29:58 +0100

    made app only be instantiated once for medium tests

--- src/test/kotlin/io/realworld/conduit/test/WebServerExtension.kt
+++ src/test/kotlin/io/realworld/conduit/test/WebServerExtension.kt
@@ -5,31 +5,39 @@ import io.restassured.RestAssured
 import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace
+import org.junit.jupiter.api.extension.ExtensionContext.Store
 import org.testcontainers.containers.PostgreSQLContainer
 
-class WebServerExtension : BeforeAllCallback, AfterAllCallback {
+class WebServerExtension : BeforeAllCallback {
     private lateinit var app: App
 
     override fun beforeAll(context: ExtensionContext) {
-        val databaseContainer = PostgreSQLContainer<Nothing>()
+        val store = rootStore(context)
+        store.getOrComputeIfAbsent("app") {
+            val databaseContainer = PostgreSQLContainer<Nothing>()
 
-        databaseContainer.start()
-        val properties = mapOf(
-            "DB_URL" to databaseContainer.jdbcUrl,
-            "DB_USER" to databaseContainer.username,
-            "DB_PASSWORD" to databaseContainer.password,
-            "APP_PORT" to 0
-        )
+            databaseContainer.start()
+            val properties = mapOf(
+                "DB_URL" to databaseContainer.jdbcUrl,
+                "DB_USER" to databaseContainer.username,
+                "DB_PASSWORD" to databaseContainer.password,
+                "APP_PORT" to 0
+            )
 
-        app = App(
-            properties = properties,
-            initializeDatabase = true
-        )
-        app.start()
-        RestAssured.port = app.port()
+            app = App(
+                properties = properties,
+                initializeDatabase = true
+            )
+            app.start()
+            RestAssured.port = app.port()
+
+            app
+        }
+
     }
 
-    override fun afterAll(context: ExtensionContext) {
-        app.stop()
+    private fun rootStore(context: ExtensionContext): Store {
+        return context.root.getStore(Namespace.create(WebServerTest::class.java))
     }
 }
