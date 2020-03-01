b3e92170853b51f67491bd91ea6cb8814d19025b
david.hernando <david.hernandojurado@mytheresa.com>
2020-03-01 20:52:20 +0100

    remove role from the jwt

--- src/main/kotlin/io/realworld/conduit/user/infrastructure/auth/JWTTokenCreator.kt
+++ src/main/kotlin/io/realworld/conduit/user/infrastructure/auth/JWTTokenCreator.kt
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.realworld.conduit.user.domain.TokenCreator
 import io.realworld.conduit.user.domain.UserId
-import io.realworld.conduit.user.infrastructure.api.Roles
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.Date
@@ -15,7 +14,6 @@ class JWTTokenCreator(private val algorithm: Algorithm) : TokenCreator {
             .withIssuer("conduit")
             .withExpiresAt(Date.from(Instant.now().plus(5, ChronoUnit.DAYS)))
             .withSubject(userId.value.toString())
-            .withClaim("role", Roles.AUTH.toString())
             .sign(algorithm)
     }
 }
